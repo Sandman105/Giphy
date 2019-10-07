d6aaa62cf071d1194380b900edb376a2fe7c33ca
Sandman105 <cpossandon@gmail.com>
2019-10-07 00:05:53 -0400

    starting buttons click function working and still images being requested from API

--- assets/giphy.js
+++ assets/giphy.js
@@ -1,41 +1,62 @@
 //VARIABLES ==================================================================
 
 //Made the empty array called "topics" Need to put some strings related to the topic that interests me. So I did the API search already for Anthony Bourdain, now I need to think of whatever other topics. 
-var topics = ["Queens Of The Stone Age", "The Raconteurs", "Kurt Vile and The Violators", "The Dead Weather", "The Kills", "Eagles Of Death Metal", "Iggp Pop", "Anthony Bourdain"];
+var topics = ["Queens Of The Stone Age", "The Raconteurs", "Kurt Vile", "The Dead Weather", "The Kills", "Eagles Of Death Metal", "Iggp Pop", "Anthony Bourdain"];
 
-//$("#button-view").on("click", function () {
+$("button").on("click", function () {
 
-//var queryParam = { "apiKey": "MrAcjsEMyvdkFPuwhAIhH1MxVGfHbjWi" };
-var queryURl = "https://api.giphy.com/v1/gifs/search?q=queens+of+the+stone+age&limit=10&rating=g&apikey=MrAcjsEMyvdkFPuwhAIhH1MxVGfHbjWi";
+    var x = $(this).data("search");
+
+    //To make sure we know which button is pushed, now each button in the HTML has a data-search tag. I do like the more dynamic way the for loop pushed the names to the <button>. Will try this for now and maybe revert once it becomes a little clearer.
+
+    console.log(x);
+
+    var queryURl = "https://api.giphy.com/v1/gifs/search?q=" + x + "&apikey=MrAcjsEMyvdkFPuwhAIhH1MxVGfHbjWi&limit=10&rating=g&";
+
+    console.log("---------------");
+    console.log(queryURl);
+
+    $.ajax({
+        url: queryURl,
+        method: "GET"
+    })
+
+        .then(function (response) {
+            console.log(response.data[0].images.downsized.url);
+
+            $(".rating").append("<p> Rating: " + response.data[0].rating + "</p>");
+            $(".band-image").append("<img src=' " + response.data[0].images.fixed_height_still.url + "'>'");
+
+        });
+
+    //var queryParam = { "apiKey": "MrAcjsEMyvdkFPuwhAIhH1MxVGfHbjWi" };
 
 
-$.ajax({
-    url: queryURl,
-    method: "GET"
-})
-
-    .then(function (response) {
-        console.log(response);
-
-        var imageURL = response.data.images;
-
-        var rockBands = $("<img>");
-
-        rockBands.attr("src", imageURL);
-        rockBands.attr("alt", "QOTSA");
-
-        $("images").append(rockBands);
 
 
 
 
-    });
+
+    /* var imageURL = response.data.images;
+
+    var rockBands = $("<img>");
+
+    rockBands.attr("src", imageURL);
+    rockBands.attr("alt", "QOTSA");
+
+    $("images").append(rockBands); */
 
 
 
-    //$("#buttons-view").empty();
 
-    for (var i = 0; i < topics.length; i++) {
+});
+
+
+
+//$("#buttons-view").empty();
+
+
+/* for (var i = 0; i < topics.length; i++) {
 
     var a = $("<button>");
     a.addClass("bands");
@@ -43,7 +64,7 @@ $.ajax({
     a.text(topics[i]);
     $("#buttons-view").append(a);
 
-    }
+} */
 
 
 
@@ -58,4 +79,4 @@ $.ajax({
     //for loop buttons come up dynamically when I pulled them out of my initialize function, I've done this one before, but the buttons are working.
 
 
-//});
+
